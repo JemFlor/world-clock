3861d1f19bfc4b7af2a67bbb08abd9b1927dff24
Jemima M <117868405+JemFlor@users.noreply.github.com>
2023-02-24 15:15:41 +0000

    Updated JS

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let timeZone = event.target.value;
+  let cityName = timeZone.replace("_", "").split("/")[1];
+  let cityTime = moment().tz(timeZone);
+  let citiesElement = document.querySelector("#overall");
+  citiesElement.innerHTML = `
+     <h2>${cityName}</h2>
+
+      <h3>${cityTime.format("MMMM Do YYYY")}</h3>
+      <div class="container" style="text-align: center"></div>
+      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+    `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
