f87ac74bd1bdd4588fff681773c961f5246f60fa
Jemima M <117868405+JemFlor@users.noreply.github.com>
2023-02-24 12:13:39 +0000

    Updated

---
+++ index.js
@@ -0,0 +1,13 @@
+function updateTime() {
+  let selectedElement = document.querySelector("#selected");
+  if (selectedElement) {
+    let selectedDateElement = selectedElement.querySelector("#date");
+    let selectedTimeElement = selectedElement.querySelector("#time");
+    let selectedTime = moment().tz("Europe/London");
+
+    selectedDateElement.innerHTML = selectedTime.format("MMMM Do YYYY");
+    selectedTimeElement.innerHTML = selectedTime.format(
+      "h:mm:ss [<small]A[</small>]"
+    );
+  }
+}
